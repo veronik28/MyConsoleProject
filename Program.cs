363ccad48dfe8482e288bed31f5a1b0e1085aaa9
veronik28 <is25kushevarovavn@artcollege.ru>
2024-03-20 21:41:35 +0300

    второй коммит

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MyConsoleProject;
+
+class Program
+{
+     static void Main(string[] args)
+    {
+        // работу выполнили Кушеварова и Суслинников
+
+        Console.WriteLine("Введите значение x:");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        double result = 5 * Math.Cos(2 * x);
+
+        Console.WriteLine($"Результат вычисления функции 5Cos(2x) для x = {x}: {result}");
+    }
+
+}
